fe96be649fb6a166d209c39426cceab3d63800da
Vitinho <vitorcbs18@gmail.com>
2023-05-17 09:06:26 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { VideoListComponent } from './video-list/video-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: InicioComponent
+  },
+  {
+    path: 'listVideo', component: VideoListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "/src/app/header/header.component.css",
+    "/src/app/header/header.component.html",
+    "/src/app/header/header.component.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
